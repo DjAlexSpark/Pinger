219e6101ded6b6ee09cb2c38fc1dcc7de83b30b8
DjAlexSpark <49067811+DjAlexSpark@users.noreply.github.com>
2024-01-06 01:50:15 +1000

    пашет

--- src/main/java/com/djalexspark/pinger/pinger/HelloApplication.java
+++ src/main/java/com/djalexspark/pinger/pinger/HelloApplication.java
@@ -3,18 +3,31 @@ package com.djalexspark.pinger.pinger;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 import java.io.IOException;
 
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("main_page.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+        HelloController controller = fxmlLoader.getController();
+//        stage.setResizable(false);
+        stage.initStyle(StageStyle.TRANSPARENT);
         stage.setTitle("Hello!");
         stage.setScene(scene);
+
+
+        //врубаем перетаскивание
+        controller.setDragndropEffect(stage);
+
+        //врубаем поток с пингом
+        controller.setPinger();
         stage.show();
+
     }
 
     public static void main(String[] args) {
--- src/main/java/com/djalexspark/pinger/pinger/HelloController.java
+++ src/main/java/com/djalexspark/pinger/pinger/HelloController.java
@@ -1,14 +1,133 @@
 package com.djalexspark.pinger.pinger;
 
+import java.io.File;
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.URL;
+import java.net.UnknownHostException;
+import java.util.ResourceBundle;
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
+import javafx.application.Platform;
+import javafx.concurrent.Task;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.ContextMenu;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.DragEvent;
+import javafx.scene.input.Dragboard;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.input.TransferMode;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.VBox;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
 
 public class HelloController {
-    @FXML
-    private Label welcomeText;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private BorderPane borderPane;
+
+    @FXML
+    private ContextMenu contextLabelMenu;
+
+    @FXML
+    private Label statusLabel;
+
+    @FXML
+    void onActionContextLabelMenu(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onBorderDragDetected(MouseEvent event) {
+
+    }
+
+    @FXML
+    void onBorderDragDropped(DragEvent event) {
+
+    }
+
+    @FXML
+    void onExitAction(ActionEvent event) {
+        System.exit(0);
+    }
+
+    @FXML
+    void onMenuMoveClick(ActionEvent event) {
+
+    }
+
+    @FXML
+    void initialize() {
+        ScheduledExecutorService executor = Executors.newScheduledThreadPool(1);
+        executor.scheduleAtFixedRate(this::checkWebsiteAvailability, 0, 5, TimeUnit.SECONDS);
+    }
+
+    private void checkWebsiteAvailability() {
+        try {
+            InetAddress address = InetAddress.getByName("8.8.8.8");
+            long startTime = System.currentTimeMillis();
+            boolean isReachable = address.isReachable(5000);
+            long endTime = System.currentTimeMillis();
+            long timeTaken = endTime - startTime;
+
+            Platform.runLater(() -> {
+                if (isReachable) {
+                    statusLabel.setText("Status: Available (Response time: " + timeTaken + "ms)");
+                } else {
+                    statusLabel.setText("Status: Unavailable");
+                }
+            });
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+
+
+
+public Stage stage;
+    double xOffset, yOffset;
+    public void setDragndropEffect(Stage stage) {
+        this.stage = stage;
+        borderPane.setOnMousePressed(event -> {
+            xOffset = event.getSceneX();
+            yOffset = event.getSceneY();
+        });
+
+        borderPane.setOnMouseDragged(event -> {
+            this.stage.setX(event.getScreenX() - xOffset);
+            this.stage.setY(event.getScreenY() - yOffset);
+        });
+
+    }
+    Task task;
+    public void setPinger() {
+        task = new Task() {
+           @Override
+           protected Object call() throws Exception {
+
+               return null;
+           }
+       };
+
     }
 }
